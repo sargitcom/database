0a922870fbffe29e2ca6fbe8ef0696e610a04d8a
LAPTOP-5NLK28AF\Kamil <kamil.pietrzkiewicz@sellplex.pl>
2018-06-20 14:39:51 +0200

    Add database connection by qrzysio pdo and add config of qrzysio pdo

--- Drivers/MysqlPdoQrzysio.php
+++ Drivers/MysqlPdoQrzysio.php
@@ -2,10 +2,35 @@
 
 namespace KamilPietrzkiewicz\Sargit\Php\Database\Schema\Drivers;
 
-class MysqlPdoQrzysio
+use KamilPietrzkiewicz\Sargit\Php\Database\Schema\InterfaceList\Database\Driver as DbDriver;
+
+class MysqlPdoQrzysio implements DbDriver 
 {
+	/**
+	 * @var Db
+	 */
+	private $dbConnection;
+	
 	public function __construct()
 	{
+		require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Configs' . DIRECTORY_SEPARATOR . 'config.php');
+	}
+
+	public function connectToDatabase(
+		string $host, 
+		string $port, 
+		string $databaseName, 
+		string $login, 
+		string $password
+	) : bool {
+		$this->db = new Db();
+	}
+	
+	public function getSchema() : DatabaseSchemaCollection
+	{
+		//$sql = 'SHOW TABLES';
+		
+		//$this->db->query($sql);
 		
 		
 	}
